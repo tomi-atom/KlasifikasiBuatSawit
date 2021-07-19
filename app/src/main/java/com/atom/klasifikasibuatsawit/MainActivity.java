a158c3ea788acaacc939cdd5cfd06e9106009719
atom <tomi.firman@student.unri.ac.id>
2021-07-19 08:54:47 +0700

    Initial commit

---
+++ app/src/main/java/com/atom/klasifikasibuatsawit/MainActivity.java
@@ -0,0 +1,37 @@
+package com.atom.klasifikasibuatsawit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.CancellationSignal;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageButton scan,info;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        scan   = findViewById(R.id.scan);
+        info = findViewById(R.id.info);
+
+        scan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ClassifierActivity.class);
+                startActivity(i);
+            }
+        });
+        info.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this, ClassifierActivity.class);
+                startActivity(i);
+            }
+        });
+    }
+}
